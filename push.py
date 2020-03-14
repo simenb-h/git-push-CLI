39d9505e9edf80e7c9ea4bfb1381d1a033a0a702
simenb-h <38004714+simenb-h@users.noreply.github.com>
2020-03-14 17:42:10 +0100

    Create push.py

---
+++ push.py
@@ -0,0 +1,12 @@
+import subprocess as cmd
+
+cp = cmd.run("git add -A ", check=True, shell=True)
+
+response = input("Default message = 'update repository'?(y/n)\n")
+message = "update repository"
+
+if response.startswith('n'):
+    message = input("Write commit message?\n")
+
+cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+cp = cmd.run("git push -u origin master -f", check=True, shell=True)
