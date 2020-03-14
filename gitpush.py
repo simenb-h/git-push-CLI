13af0a63836abbf4ef16b1140d1e7e6129881090
simenb-h <38004714+simenb-h@users.noreply.github.com>
2020-03-14 18:18:25 +0100

    Update gitpush.py

--- gitpush.py
+++ gitpush.py
@@ -3,10 +3,11 @@ import sys
 
 cp = cmd.run("git add -A ", check=True, shell=True)
 
-if (len(sys.argv)==0):
-    message = input("update the repository")
+if (len(sys.argv)-1==0):
+    message = "update the repository"
 else: 
     message = sys.argv[1]
+
 
 cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
