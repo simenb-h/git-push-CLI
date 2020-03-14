b9927882be70482ef3afdd78f38ab09cdb01b318
simenb-h <38004714+simenb-h@users.noreply.github.com>
2020-03-14 18:11:57 +0100

    Update and rename push.py to gitpush.py

--- gitpush.py
+++ gitpush.py
@@ -1,12 +1,13 @@
 import subprocess as cmd
+import sys
 
 cp = cmd.run("git add -A ", check=True, shell=True)
 
-response = input("m = 'update repository'?(y/n)\n")
-message = "update repository"
-
-if response.startswith('n'):
-    message = input("Write commit message?\n")
+if (len(sys.argv)==0):
+    message = input("update the repository")
+else: 
+    message = sys.argv[1]
 
 cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+
